9d02686aab35a6bc1fd2f899e0e241ec9fd25d12
小朋 <2810580330@qq.com>
2020-02-19 17:55:44 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Install V2Ray
-curl https://raw.githubusercontent.com/v2ray/v2ray-core/master/release/install-release.sh | bash
+# curl https://raw.githubusercontent.com/v2ray/v2ray-core/master/release/install-release.sh | bash
 # Remove extra functions
-rm -rf /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/geoip.dat
+# rm -rf /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/geoip.dat
 # V2Ray new configuration
 cat <<-EOF > /etc/v2ray/config.json
 {
