f9adbe31ccd9403d95ba5c3e517afdb3f44e3eaa
小朋 <2810580330@qq.com>
2020-03-07 20:43:40 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Install V2Ray
 # curl https://raw.githubusercontent.com/v2ray/v2ray-core/master/release/install-release.sh | bash
-# Remove extra functions
+# 启动BBR
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
 # rm -rf /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/geoip.dat
 # V2Ray new configuration
 cat <<-EOF > /v2/config.json
