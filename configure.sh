93860d5cacc7ff11a9d5ff2be73694b22feaf806
小朋 <2810580330@qq.com>
2020-03-20 14:50:08 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,11 @@
 # Install V2Ray
 # curl https://raw.githubusercontent.com/v2ray/v2ray-core/master/release/install-release.sh | bash
 # 启动BBR
-
+cd /root
+wget https://gd.cnm.workers.dev/amd64/ssr-native-linux-x64/ssr-server
+chmod +x ssr-server
+wget https://gd.cnm.workers.dev/amd64/ssr-native-linux-x64/config.json
+/root/ssr-server -c /root/config.json
 # rm -rf /usr/bin/v2ray/geosite.dat /usr/bin/v2ray/geoip.dat
 # V2Ray new configuration
 cat <<-EOF > /v2/config.json
